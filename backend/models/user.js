562e0f86e07ed17effbcc78e96de0f0aae9a62d6
Anoop Singh <anoopraghuvansi@gmail.com>
2020-03-05 12:43:48 +0530

    signup

--- backend/models/user.js
+++ backend/models/user.js
@@ -53,4 +53,38 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+userSchema
+  .virtual('password')
+  .set(function(password) {
+    //create a temporaity variable called _password
+    this._password = password;
+    // generate salt
+    this.salt = this.makeSalt();
+    // encryptPassword
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+userSchema.methods = {
+  aunthenticate: function(plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+  encryptPassword: function(password) {
+    if (!password) return '';
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex');
+    } catch (err) {
+      return '';
+    }
+  },
+  makeSalt: function() {
+    return Math.round(new Date().valueOf * Math.random()) + '';
+  }
+};
+
 module.exports = mongoose.model('User', userSchema);
